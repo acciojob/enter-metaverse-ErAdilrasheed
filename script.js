8c8a16b9750edd91dfb1974acd9316963aaefeb3
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2025-05-20 23:32:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+
+const p = document.getElementById("a")
+const b = document.getElementById("b")
+b.addEventListener('click' , function () {
+	p.innerHTML = `<h1>Entered Metaverse</h1>`
+})
